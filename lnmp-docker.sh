aa35a2cdbabfd424c350e60e3abcc25083f801ec
khs1994 <khs1994@khs1994.com>
2017-10-24 00:05:35 +0800

    Update [skip ci]

--- lnmp-docker.sh
+++ lnmp-docker.sh
@@ -4,6 +4,15 @@ ENV=$1
 ARCH=`uname -m`
 OS=`uname -s`
 
+. env/.env
+
+if [ OS = "Darwin" ];then
+  # 将以什么开头的行替换为新内容
+  sed -i '' "s/^KHS1994_LNMP_DOCKER_VERSION.*/KHS1994_LNMP_DOCKER_VERSION=${KHS1994_LNMP_DOCKER_VERSION}/g" .env
+else
+  sed -i "s/^KHS1994_LNMP_DOCKER_VERSION.*/KHS1994_LNMP_DOCKER_VERSION=${KHS1994_LNMP_DOCKER_VERSION}/g" .env
+fi
+
 # 不支持信息
 
 NOTSUPPORT(){
