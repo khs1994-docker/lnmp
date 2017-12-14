264495c028b16f45f2e7d2bc7abf63f97c9c4aa0
khs1994 <khs1994@khs1994.com>
2017-12-15 01:29:09 +0800

    Update [skip ci]

--- lnmp-docker.ps1
+++ lnmp-docker.ps1
@@ -50,66 +50,89 @@ Function init(){
 Function help_information(){
   echo "Docker-LNMP CLI ${KHS1994_LNMP_DOCKER_VERSION}
 
-Usage: ./docker-lnmp.ps1 COMMAND
+Official WebSite https://lnmp.khs1994.com
+
+Usage: ./docker-lnmp.sh COMMAND
 
 Commands:
-backup               Backup MySQL databases
-cleanup              Cleanup log files
-composer             Use PHP Package Management composer
-development          Use LNMP in Development
-development-config   Validate and view the Development(with build images)Compose file
-development-build    Use LNMP in Development With Build images
-down                 Stop and remove LNMP Docker containers, networks, images, and volumes
-help                 Display this help message
-laravel              Create a new Laravel application
-laravel-artisan      Use Laravel CLI artisan
-php                  Run PHP in CLI
-push                 Build and Pushes images to Docker Registory v2
-restore              Restore MySQL databases
+  backup               Backup MySQL databases
+  build                Use LNMP With Build images(Support x86_64)
+  build-config         Validate and view the build images Compose file
+  cleanup              Cleanup log files
+  composer             Use PHP Package Management composer
+  development          Use LNMP in Development(Support x86_64 arm32v7 arm64v8)
+  down                 Stop and remove LNMP Docker containers, networks, images, and volumes
+  docs                 Support Documents
+  help                 Display this help message
+  laravel              Create a new Laravel application
+  laravel-artisan      Use Laravel CLI artisan
+  php                  Run PHP in CLI
+  production           Use LNMP in Production(Only Support Linux x86_64)
+  production-config    Validate and view the Production Compose file
+  push                 Build and Pushes images to Docker Registory v2
+  restore              Restore MySQL databases
+  swarm-build          Build Swarm image (nginx php7)
+  swarm-push           Push Swarm image (nginx php7)
+  swarm-deploy         Deploy LNMP stack TO Swarm mode
+  swarm-down           Remove LNMP stack IN Swarm mode
 
 Container CLI:
-memcached-cli
-mongo-cli
-mysql-cli
-nginx-cli
-php-cli
-postgres-cli
-rabbitmq-cli
-redis-cli
+  memcached-cli
+  mongo-cli
+  mysql-cli
+  nginx-cli
+  php-cli
+  postgres-cli
+  rabbitmq-cli
+  redis-cli
 
 Tools:
-update                Upgrades LNMP
+  update                Upgrades LNMP
+  upgrade               Upgrades LNMP
+  init
+  commit
+  test
+  dockerfile-update    Update Dockerfile By Script
+  debug                Debug environment
+  cn-mirror            Push master branch to CN mirror
+  compose              Install docker-compose By curl from github
 
 Read './docs/*.md' for more information about commands."
 }
 
 Function backup(){
-
+  docker-compose exec mysql /backup/backup.sh $args[1] $args[2] $args[3]
 }
 
 Function cleanup(){
-  Write-Host " " | Out
+  Write-Host " "
+  rm logs\mongodb -Recurse -Force
+  rm logs\mysql -Recurse -Force
+  rm logs\nginx -Recurse -Force
+  rm logs\php-fpm -Recurse -Force
+  rm logs\redis -Recurse -Force
+  logs
 }
 
 Function composer($COMPOSE_PATH,$CMD){
-  Write-Host "IN khs1994/php-fpm:${KHS1994_LNMP_PHP_VERSION}-alpine  /app/${COMPOSE_PATH} EXEC $ composer ${CMD}"
+  Write-Host "IN khs1994/php-fpm:${KHS1994_LNMP_PHP_VERSION}-alpine3.7  /app/${COMPOSE_PATH} EXEC $ composer ${CMD}"
   Write-Host "output information"
   Write-Host " "
-  docker run -it --rm -v $pwd\app\${COMPOSE_PATH}:/app -v $pwd\tmp\cache:/tmp/cache khs1994/php-fpm:${KHS1994_LNMP_PHP_VERSION}-alpine composer ${CMD}
+  docker run -it --rm -v $pwd\app\${COMPOSE_PATH}:/app -v $pwd\tmp\cache:/tmp/cache khs1994/php-fpm:${KHS1994_LNMP_PHP_VERSION}-alpine3.7 composer ${CMD}
 }
 
 Function laravel($LARAVEL_PATH){
-  Write-Host "IN khs1994/php-fpm:${KHS1994_LNMP_PHP_VERSION}-alpine  /app/ EXEC $ laravel new ${LARAVEL_PATH}"
+  Write-Host "IN khs1994/php-fpm:${KHS1994_LNMP_PHP_VERSION}-alpine3.7  /app/ EXEC $ laravel new ${LARAVEL_PATH}"
   Write-Host "output information"
   Write-Host " "
-  docker run -it --rm -v $pwd\app:/app -v $pwd\tmp\cache:/tmp/cache khs1994/php-fpm:${KHS1994_LNMP_PHP_VERSION}-alpine laravel new ${LARAVEL_PATH}
+  docker run -it --rm -v $pwd\app:/app -v $pwd\tmp\cache:/tmp/cache khs1994/php-fpm:${KHS1994_LNMP_PHP_VERSION}-alpine3.7 laravel new ${LARAVEL_PATH}
 }
 
 Function laravel-artisan($LARAVEL_PATH,$CMD){
-  Write-Host "IN khs1994/php-fpm:${KHS1994_LNMP_PHP_VERSION}-alpine  /app/${LARAVEL_PATH} EXEC $ php artisan ${CMD}"
+  Write-Host "IN khs1994/php-fpm:${KHS1994_LNMP_PHP_VERSION}-alpine3.7  /app/${LARAVEL_PATH} EXEC $ php artisan ${CMD}"
   Write-Host "output information"
   Write-Host " "
-  docker run -it --rm -v $pwd\app\${LARAVEL_PATH}:/app khs1994/php-fpm:${KHS1994_LNMP_PHP_VERSION}-alpine php artisan ${CMD}
+  docker run -it --rm -v $pwd\app\${LARAVEL_PATH}:/app khs1994/php-fpm:${KHS1994_LNMP_PHP_VERSION}-alpine3.7 php artisan ${CMD}
 }
 
 Function update(){
@@ -148,11 +171,11 @@ Function main() {
     }
 
     backup {
-
+      backup
     }
 
     cleanup {
-
+      cleanup
     }
 
     composer {
@@ -164,16 +187,25 @@ Function main() {
       docker-compose up -d
     }
 
-    development-config{
+    build-config{
       docker-compose -f docker-compose.yml -f docker-compose.build.yml config
     }
 
-    development-build{
+    build{
       docker-compose -f docker-compose.yml -f docker-compose.build.yml up -d
+    }
+
+    push {
+      docker-compose -f docker-compose.yml -f docker-compose.build.yml build
+      docker-compose -f docker-compose.yml -f docker-compose.build.yml push
     }
 
     down {
       docker-compose down
+    }
+
+    docs {
+      docker run -it --rm -p 4000:4000 -v $pwd\docs:/srv/gitbook-src khs1994/gitbook server
     }
 
     help {
@@ -188,22 +220,34 @@ Function main() {
       laravel-artisan $args[1] $args[2]
     }
 
-    mysql-demo {
-
-    }
-
     php {
       $PHP_PATH=$args[1]
       $PHP_FILE=$args[2]
-      docker run -it --rm -v $pwd\app\${PHP_PATH}:/app khs1994/php-fpm:${KHS1994_LNMP_PHP_VERSION}-alpine php $PHP_FILE
+      docker run -it --rm -v $pwd\app\${PHP_PATH}:/app khs1994/php-fpm:${KHS1994_LNMP_PHP_VERSION}-alpine3.7 php $PHP_FILE
     }
 
-    push {
-
+    production-config {
+       docker-compose -f docker-compose.yml -f docker-compose.prod.yml config
     }
 
     restore {
+       docker-compose exec mysql /backup/restore.sh $args[1]
+    }
 
+    swarm-build {
+      docker-compose -f docker-stack.yml build nginx php7
+    }
+
+    swarm-push {
+      docker-compose -f docker-stack.yml push nginx php7
+    }
+
+    swarm-deploy {
+      docker stack deploy -c docker-stack.yml lnmp
+    }
+
+    swarm-down {
+      docker stack rm lnmp
     }
 
     memcached-cli {
