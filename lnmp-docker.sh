a494bee8b40db88b160d5ea75b6b66f9d13e9f49
khs1994 <khs1994@khs1994.com>
2017-12-15 13:53:57 +0800

    Update [skip ci]

--- lnmp-docker.sh
+++ lnmp-docker.sh
@@ -299,7 +299,7 @@ update(){
     if [ ! -z "${GIT_STATUS}" ];then git status -s --ignore-submodules; echo; print_error "Please commit then update"; exit 1; fi
   fi
   git fetch lnmp
-  print_info "Branch is ${BRANCH}\n"
+  print_info "\nBranch is ${BRANCH}\n"
   if [ ${BRANCH} = "dev" ];then
     git reset --hard lnmp/dev
   elif [ ${BRANCH} = "master" ];then
