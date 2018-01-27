61c7d11ccad41217c8299bc1662b4c14ba6ce621
khs1994 <khs1994@khs1994.com>
2018-01-27 22:19:18 +0800

    Update [skip ci]

--- kubernetes/kubernetes.sh
+++ kubernetes/kubernetes.sh
@@ -8,7 +8,7 @@ kubectl delete service -l app=lnmp
 
 kubectl delete pvc -l app=lnmp
 
-kubectl delete pv lnmp-mysql-data lnmp-redis-data lnmp-data
+kubectl delete pv lnmp-mysql-data lnmp-redis-data lnmp-data lnmp-nginx-conf
 
 kubectl delete secret lnmp-mysql-password
 
