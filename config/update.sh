183fd347c0bc6caeb234bf8ab40b41947135dfd5
khs1994 <khs1994@khs1994.com>
2018-02-26 22:26:21 +0800

    Update

--- config/update.sh
+++ config/update.sh
@@ -4,6 +4,8 @@
 
 wget https://gitee.com/mirrors/redis/raw/${KHS1994_LNMP_REDIS_VERSION}/redis.conf -O redis/redis.conf
 
+cp redis/redis.conf redis/redis.production.conf
+
 curl -L https://raw.githubusercontent.com/php/php-src/php-${KHS1994_LNMP_PHP_VERSION}/php.ini-production > php/php.production.ini
 
 curl -L https://raw.githubusercontent.com/php/php-src/php-${KHS1994_LNMP_PHP_VERSION}/php.ini-development > php/php.development.ini
