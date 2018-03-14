eabddf50869dfa1102a67b75ac0c058987229660
khs1994 <khs1994@khs1994.com>
2018-03-15 01:10:12 +0800

    Update mysql case

--- cluster/mysql/master/master.sh
+++ cluster/mysql/master/master.sh
@@ -8,9 +8,14 @@ export MYSQL_PWD=${DB_ROOT_PASSWORD}
 
 # MySQL Docker 8.0.4 based stretch(Debian 9) Remove ip ping etc command
 
+# new user
+
+mysql -u root \
+-e "GRANT ALL PRIVILEGES ON *.* TO 'master'@'%' IDENTIFIED BY '${DB_ROOT_PASSWORD}' WITH GRANT OPTION;"
+
 # create replication user
 
 # mysql_net=$(ip route | awk '$1=="default" {print $3}' | sed "s/\.[0-9]\+$/.%/g")
 
-mysql -uroot \
--e "GRANT REPLICATION SLAVE ON *.* TO 'backup'@'172.28.0.%' IDENTIFIED BY 'mytest';"
+mysql -u root \
+-e "GRANT REPLICATION SLAVE ON *.* TO '${MYSQL_REPLICATION_USER}'@'172.28.0.%' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
--- cluster/mysql/node/node.sh
+++ cluster/mysql/node/node.sh
@@ -10,16 +10,42 @@ DB_ROOT_PASSWORD=$(cat /etc/mysql/db_root_password)
 
 export MYSQL_PWD=${DB_ROOT_PASSWORD}
 
-# check mysql master run status
+# check mysql master status
 
-until mysql -u root -h mysql_master ; do
+until mysql -umaster -h mysql_master ; do
   >&2 echo "MySQL master is unavailable - sleeping"
   sleep 3
 done
 
+echo "MySQL master is available"
+
+# new only read user
+
+mysql -u root \
+-e "GRANT SELECT ON *.* TO 'node'@'%' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+
+# new REPLICATION user
+
+mysql -u root \
+-e "GRANT REPLICATION SLAVE ON *.* TO '${MYSQL_REPLICATION_USER}'@'172.28.0.%' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+
+# 锁定数据库
+
+mysql -uroot -h mysql_master -e "FLUSH TABLES WITH READ LOCK"
+
+# 备份数据库
+
+# mysqldump -u root -p${DB_ROOT_PASSWORD} --all-databases  --lock-tables=false  -- > /root/all.sql
+
 # get master log File & Position
 
 master_status_info=$(mysql -u root -h mysql_master -e "show master status\G")
+
+# 解锁数据库
+
+mysql -uroot -h mysql_master -e "UNLOCK TABLES"
+
+# 解析值
 
 LOG_FILE=$(echo "${master_status_info}" | awk 'NR!=1 && $1=="File:" {print $2}')
 
@@ -29,7 +55,8 @@ LOG_POS=$(echo "${master_status_info}" | awk 'NR!=1 && $1=="Position:" {print $2
 
 mysql -u root \
 -e "CHANGE MASTER TO MASTER_HOST='mysql_master', \
-MASTER_USER='backup', \
+MASTER_PORT=3306, \
+MASTER_USER='${MYSQL_REPLICATION_USER}', \
 MASTER_PASSWORD='${DB_ROOT_PASSWORD}', \
 MASTER_LOG_FILE='${LOG_FILE}', \
 MASTER_LOG_POS=${LOG_POS}"
@@ -40,8 +67,8 @@ mysql -u root -e "START SLAVE;show slave status\G"
 
 # 主节点新建数据库
 
-mysql -uroot -h mysql_master -e 'create database if not exists test'
+mysql -umaster -h mysql_master -e 'create database if not exists test'
 
 # 从节点验证
 
-mysql -uroot -e 'show databases;'
+mysql -unode -e 'show databases;'
