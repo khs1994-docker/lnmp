f7cf5ae06c6361631ef5ed7f17c4712b995ecbda
khs1994 <khs1994@khs1994.com>
2018-03-23 00:48:52 +0800

    Update kubernetes

--- kubernetes/minikube.ps1
+++ kubernetes/minikube.ps1
@@ -1,1 +1,6 @@
-minikube.exe start --mount-string="C:\Users\90621:/minikube-host" -v 10 --registry-mirror=https://registry.docker-cn.com --vm-driver="hyperv" --memory=4096
+minikube.exe start `
+    --mount-string="C:\Users\90621:/minikube-host" `
+    -v 10 `
+    --registry-mirror=https://registry.docker-cn.com `
+    --vm-driver="hyperv" `
+    --memory=4096
