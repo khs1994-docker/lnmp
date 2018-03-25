c7b4428a7d3d1bca20b6f81aec2e85c54596d881
khs1994 <khs1994@khs1994.com>
2018-03-26 00:57:04 +0800

    update

--- windows/lnmp-laravel.ps1
+++ windows/lnmp-laravel.ps1
@@ -4,11 +4,23 @@ if ($args -contains 'new' ){
   }
 }
 
-docker run --init -it --rm `
-    --mount type=bind,src=$PWD,target=/app `
-    --mount src=lnmp_composer_cache-data,target=/tmp/cache `
-    khs1994/php-fpm:7.2.3-alpine3.7 `
-    laravel $args
+if (!(Test-Path ${LARAVEL_PATH})){
+  echo ""
+  echo "${LARAVEL_PATH} not existing"
+  echo ""
+# docker run --init -it --rm `
+#     --mount type=bind,src=$PWD,target=/app `
+#     --mount src=lnmp_composer_cache-data,target=/tmp/cache `
+#     khs1994/php-fpm:7.2.3-alpine3.7 `
+#     laravel $args
+
+composer create-project --prefer-dist laravel/laravel=5.6.* "$LARAVEL_PATH"
+
+}else{
+  echo ""
+  echo "${LARAVEL_PATH} existing"
+  echo ""
+}
 
 if ($args -contains 'new' ){
   $global:LARAVEL_PATH=$args[1]
@@ -23,7 +35,7 @@ if ($args -contains 'new' ){
 
   echo "install laravel-ide-helper ..."
 
-  lnmp-composer require --dev barryvdh/laravel-ide-helper
+  composer require --dev barryvdh/laravel-ide-helper
 
   Write-Host "
 
@@ -49,7 +61,7 @@ $ lnmp-php artisan optimize
 "
 
   echo ".phpstorm.meta.php
-  _ide_helper.php" >> .gitignore
+_ide_helper.php" >> .gitignore
 
 cd ..
 
--- windows/lnmp-npm.ps1
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm `
-    --mount type=bind,src=$pwd,target=/app `
-    --workdir /app `
-    node:alpine `
-    $args
