b83f7875a9bdbe3df04af6b0cd21c555fd06978d
khs1994 <khs1994@khs1994.com>
2018-04-10 09:44:32 +0800

    Update PHPer command travis

--- dockerfile/travis/docker-entrypoint.sh
+++ dockerfile/travis/docker-entrypoint.sh
@@ -4,8 +4,10 @@ if [ "$1" = 'bash' ] || [ "$1" = 'sh' ];then
   exec sh
 fi
 
+if ! [ -f /root/.travis/config.yml ];then
+
 travis whoami || travis login --github-token $GITHUB_TOKEN
 
-echo $@
+fi
 
 exec travis "$@"
