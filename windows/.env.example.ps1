0908071656d809baebbf05bce8f61bae472aa31b
khs1994 <khs1994@khs1994.com>
2018-06-06 17:07:02 +0800

    Support Gogs

--- windows/.env.example.ps1
+++ windows/.env.example.ps1
@@ -15,13 +15,13 @@ $DistributionName="debian"
 
 $COMMON_SOFT="nginx","php","mysql","wsl-redis"
 
-$global:NGINX_VERSION="1.13.12"
+$global:NGINX_VERSION="1.15.0"
 # https://windows.php.net/download/
 $global:PHP_VERSION="7.2.6"
 $global:MYSQL_VERSION="8.0.11"
 $global:HTTPD_VERSION="2.4.33"
 $global:IDEA_VERSION="1.8.3678"
-$global:NODE_VEERSION="10.2.1"
+$global:NODE_VEERSION="10.3.0"
 $global:GIT_VERSION="2.17.0"
 $global:PYTHON_VERSION="3.6.5"
 $global:GOLANG_VERSION="1.10.2"
