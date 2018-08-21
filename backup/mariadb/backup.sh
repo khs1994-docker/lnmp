812010cb7d79be464df00a2eb70c81da73648e58
khs1994 <khs1994@khs1994.com>
2018-08-21 10:48:30 +0800

    Update script

--- backup/mariadb/backup.sh
+++ backup/mariadb/backup.sh
@@ -3,10 +3,10 @@
 set -ex
 
 if [ ! -n "$1" ] ;then
-  echo -e "请传入参数
-DATABASE_NAME [tables]
---databases [OPTIONS] DB1 [DB2 DB3...]
---all-databases [OPTIONS]
+  echo -e "Usage: lnmp-backup [OPTIONS] database [tables]
+OR     lnmp-backup [OPTIONS] --databases [OPTIONS] DB1 [DB2 DB3...]
+OR     lnmp-backup [OPTIONS] --all-databases [OPTIONS]
+For more options, use lnmp-backup --help
 "
 else
   mysqldump -uroot -p"$(< /run/secrets/db_root_password)" "$@" > /backup/"$(date "+%Y%m%d-%H.%M")".sql
--- backup/mysql/backup.sh
+++ backup/mysql/backup.sh
@@ -3,11 +3,10 @@
 set -ex
 
 if [ ! -n "$1" ] ;then
-  echo -e "command is empty, please input something
-
-DATABASE_NAME [tables]
---databases [OPTIONS] DB1 [DB2 DB3...]
---all-databases [OPTIONS]
+  echo -e "Usage: lnmp-backup [OPTIONS] database [tables]
+OR     lnmp-backup [OPTIONS] --databases [OPTIONS] DB1 [DB2 DB3...]
+OR     lnmp-backup [OPTIONS] --all-databases [OPTIONS]
+For more options, use lnmp-backup --help
 "
 else
   mysqldump -uroot -p"$(< /run/secrets/db_root_password)" "$@" > /backup/"$(date "+%Y%m%d-%H.%M")".sql
