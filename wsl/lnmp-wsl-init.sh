2ec2fb98313be390aaec02a7a81e418bb2283eb6
khs1994 <khs1994@khs1994.com>
2018-09-12 21:22:32 +0800

    Update WSL init [skip ci]

--- wsl/lnmp-wsl-init.sh
+++ wsl/lnmp-wsl-init.sh
@@ -11,8 +11,7 @@ sudo sed -i "s#archive.ubuntu.com#mirrors.ustc.edu.cn#g" /etc/apt/sources.list
 sudo sed -i "s#security.ubuntu.com#mirrors.ustc.edu.cn#g" /etc/apt/sources.list
 
 # conf wsl
-curl https://raw.githubusercontent.com/khs1994-docker/lnmp/master/wsl/wsl.conf \
-  | sudo tee /etc/wsl.conf
+sudo curl https://raw.githubusercontent.com/khs1994-docker/lnmp/master/wsl/wsl.conf -o /etc/wsl.conf
 
 if [ -z $APP_ENV ];then
   echo "export APP_ENV=wsl" >> ~/.bash_profile
@@ -38,11 +37,28 @@ sudo apt update && sudo apt -y install \
                             python3 \
                             python3-pip \
                             openssh-server \
-                            apt-file
-
+                            apt-file \
+                            man
 
 # composer cn mirror
 
 command -v /usr/local/bin/composer && composer config -g repo.packagist composer https://packagist.laravel-china.org
 
-lnmp-wsl-docker-cli.sh
+# lnmp-wsl-docker-cli.sh
+# 18.03 +
+sudo ln -sf "/c/Program Files/Docker/Docker/resources/bin/docker.exe" /usr/local/bin/docker
+sudo ln -sf "/c/Program Files/Docker/Docker/resources/bin/docker-compose.exe" /usr/local/bin/docker-compose
+
+# 命令行补全
+
+if ! [ -d /etc/bash_completion.d ];then sudo mkdir -p /etc/bash_completion.d; fi
+
+if ! [ -f /etc/bash_completion.d/docker ];then
+
+sudo curl -L https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker
+
+fi
+
+# verify
+
+docker --version
