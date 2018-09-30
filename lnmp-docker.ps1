04bb29d5d357cb973c317f95f3b6c73f7919c28f
khs1994 <khs1994@khs1994.com>
2018-09-30 14:20:57 +0800

    clone cookbooks from gitee.com [replace github.com]

--- lnmp-docker.ps1
+++ lnmp-docker.ps1
@@ -765,35 +765,35 @@ XXX
 
     "cookbooks" {
       if(!(Test-Path ${APP_ROOT}/lnmp-docs/k8s)){
-        git clone --depth=1 -b gh-pages git@github.com:khs1994-website/kubernetes-handbook.git ${APP_ROOT}/lnmp-docs/k8s
+        git clone --depth=1 -b gh-pages git@gitee.com:khs1994-website/kubernetes-handbook.git ${APP_ROOT}/lnmp-docs/k8s
       }else{
         git -C ${APP_ROOT}/lnmp-docs/k8s fetch --depth=1 origin gh-pages
         git -C ${APP_ROOT}/lnmp-docs/k8s reset --hard origin/gh-pages
       }
 
       if(!(Test-Path ${APP_ROOT}/lnmp-docs/docker)){
-        git clone --depth=1 -b pages git@github.com:yeasy/docker_practice.git ${APP_ROOT}/lnmp-docs/docker
+        git clone --depth=1 -b pages git@github.com:docker_practice/docker_practice.git ${APP_ROOT}/lnmp-docs/docker
       }else{
         git -C ${APP_ROOT}/lnmp-docs/docker fetch --depth=1 origin pages
         git -C ${APP_ROOT}/lnmp-docs/docker reset --hard origin/pages
       }
 
       if(!(Test-Path ${APP_ROOT}/lnmp-docs/laravel)){
-        git clone --depth=1 -b gh-pages git@github.com:khs1994-website/laravel5.5-docs.zh-cn.git ${APP_ROOT}/lnmp-docs/laravel
+        git clone --depth=1 -b gh-pages git@gitee.com:khs1994-website/laravel5.5-docs.zh-cn.git ${APP_ROOT}/lnmp-docs/laravel
       }else{
         git -C ${APP_ROOT}/lnmp-docs/laravel fetch --depth=1 origin gh-pages
         git -C ${APP_ROOT}/lnmp-docs/laravel reset --hard origin/gh-pages
       }
 
       if(!(Test-Path ${APP_ROOT}/lnmp-docs/laravel-en)){
-        git clone --depth=1 -b gh-pages git@github.com:khs1994-website/laravel-docs.git ${APP_ROOT}/lnmp-docs/laravel-en
+        git clone --depth=1 -b gh-pages git@gitee.com:khs1994-website/laravel-docs.git ${APP_ROOT}/lnmp-docs/laravel-en
       }else{
         git -C ${APP_ROOT}/lnmp-docs/laravel-en fetch --depth=1 origin gh-pages
         git -C ${APP_ROOT}/lnmp-docs/laravel-en reset --hard origin/gh-pages
       }
 
       if(!(Test-Path ${APP_ROOT}/lnmp-docs/nginx)){
-        git clone --depth=1 -b gh-pages git@github.com:khs1994-website/nginx-docs.zh-cn.git ${APP_ROOT}/lnmp-docs/nginx
+        git clone --depth=1 -b gh-pages git@gitee.com:khs1994-website/nginx-docs.zh-cn.git ${APP_ROOT}/lnmp-docs/nginx
       }else{
         git -C ${APP_ROOT}/lnmp-docs/nginx fetch --depth=1 origin gh-pages
         git -C ${APP_ROOT}/lnmp-docs/nginx reset --hard origin/gh-pages
