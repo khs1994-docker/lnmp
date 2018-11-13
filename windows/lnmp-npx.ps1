15451a8658af7a7e244223016c00d31b424799f0
khs1994 <khs1994@khs1994.com>
2018-11-13 17:38:24 +0800

    PHP 7.1.24 [skip ci]

--- windows/lnmp-npx.ps1
+++ windows/lnmp-npx.ps1
@@ -2,6 +2,6 @@ docker run -it --rm `
     --mount type=bind,src=$PWD,target=/app `
     --mount type=volume,src=lnmp_npm-data,target=/tmp/node/.npm `
     --workdir /app `
-    --entrypoint npm `
+    --entrypoint npx `
     node:alpine `
     --registry https://registry.npm.taobao.org --cache /tmp/node/.npm $args
