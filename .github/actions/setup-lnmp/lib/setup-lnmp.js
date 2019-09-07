17f56fb537823f201bfb4c7f30a73069cf30d56d
Kang HuaiShuai <khs1994@khs1994.com>
2019-09-07 12:25:40 +0800

    [Actions] Fixed setup-lnmp

--- .github/actions/setup-lnmp/lib/setup-lnmp.js
+++ .github/actions/setup-lnmp/lib/setup-lnmp.js
@@ -1,10 +1,14 @@
 const exec = require('@actions/exec');
 const core = require('@actions/core');
+const tc = require('@actions/tool-cache');
+const io = require('@actions/io');
+
 const os = require('os');
 
 const LNMP_BRANCH = core.getInput('lnmp_branch');
 const LNMP_SERVICES = core.getInput('lnmp_services');
 const LREW_INCLUDE = core.getInput('lrew_include');
+const DOCKER_COMPOSE_VERSION = core.getInput('docker_compose_version');
 
 async function run() {
   const platform = os.platform();
@@ -34,12 +38,12 @@ async function run() {
   core.addPath('/home/runner/lnmp/bin');
   core.addPath('/home/runner/lnmp/wsl');
 
-  core.debug('init lnmp')
-  core.exportVariable('quite','true');
-  await exec.exec('lnmp-docker');
-  await exec.exec('lnmp-docker',['services']);
-  await exec.exec('lnmp-docker', ['compose', '--official']);
-  core.exportVariable('quite','');
+  core.debug('download docker compose');
+  const composePath = await tc.downloadTool(`https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-Linux-x86_64`);
+  await io.cp(composePath, '/home/runner/lnmp/bin/docker-compose');
+  await exec.exec('chmod', ['+x', '/home/runner/lnmp/bin/docker-compose']);
+
+  core.debug('stop mysql')
   await exec.exec('sudo', ['systemctl', 'stop', 'mysql']);
 }
 
