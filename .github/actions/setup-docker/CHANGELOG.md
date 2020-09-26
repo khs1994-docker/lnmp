fa1feec477ae0e4be216363f450f452bdaa086f9
Kang Huaishuai <khs1994@khs1994.com>
2020-09-26 11:06:45 +0800

    setup-docker support macos [skip ci] [sync action]
    Signed-off-by: Kang Huaishuai <khs1994@khs1994.com>

---
+++ .github/actions/setup-docker/CHANGELOG.md
@@ -0,0 +1,9 @@
+# v1
+
+## 1.0.1
+
+* support macOS https://github.com/docker/for-mac/issues/2359#issuecomment-607154849
+
+## 1.0.0
+
+* nightly version use https://github.com/AkihiroSuda/moby-snapshot
--- .github/actions/setup-docker/lib/setup-docker.js
+++ .github/actions/setup-docker/lib/setup-docker.js
@@ -11,6 +11,8 @@ const DOCKER_NIGHTLY_VERSION = core.getInput('docker_nightly_version');
 
 const systemExec = require('child_process').exec;
 
+let message;
+
 async function shell(cmd) {
   return await new Promise((resolve, reject) => {
     systemExec(cmd, function (error, stdout, stderr) {
@@ -27,34 +29,189 @@ async function shell(cmd) {
   });
 }
 
+async function buildx() {
+  core.debug('set DOCKER_CLI_EXPERIMENTAL');
+  if (DOCKER_CLI_EXPERIMENTAL === 'enabled') {
+    core.exportVariable('DOCKER_CLI_EXPERIMENTAL', 'enabled');
+  }
+
+  if (DOCKER_BUILDX !== 'true') {
+    core.info('buildx disabled');
+
+    return;
+  }
+
+  core.exportVariable('DOCKER_CLI_EXPERIMENTAL', 'enabled');
+
+  await exec.exec('docker', [
+    'buildx',
+    'version',
+  ]).then(async () => {
+    // install buildx
+    core.startGroup('setup qemu');
+    await exec.exec('docker', [
+      'run',
+      '--rm',
+      '--privileged',
+      'tonistiigi/binfmt:latest',
+      "--install",
+      "all"
+    ]);
+    core.endGroup();
+
+    core.startGroup('list /proc/sys/fs/binfmt_misc');
+    await exec.exec('ls -la', [
+      '/proc/sys/fs/binfmt_misc',
+    ]).catch(() => { });
+    core.endGroup();
+
+    core.startGroup('create buildx instance');
+    await exec.exec('docker', [
+      'buildx',
+      'create',
+      '--use',
+      '--name',
+      'mybuilder',
+      '--driver',
+      'docker-container',
+      '--driver-opt',
+      'image=moby/buildkit:master'
+    ]);
+    core.endGroup();
+
+    core.startGroup('inspect buildx instance');
+    await exec.exec('docker', [
+      'buildx',
+      'inspect',
+      '--bootstrap'
+    ]);
+    core.endGroup();
+  }, () => {
+    core.info('this docker version NOT Support Buildx');
+  });
+}
+
 async function run() {
   const platform = os.platform();
 
-  if (platform !== 'linux') {
+  if (platform === 'win32') {
     core.debug('check platform');
     await exec.exec('echo',
-      [`Only Support linux platform, this platform is ${os.platform()}`]);
+      [`Only Support Linux and macOS platform, this platform is ${os.platform()}`]);
+
     return
   }
 
+  if (platform === 'darwin') {
+    // macos
+    await exec.exec('docker', [
+      '--version']).catch(() => { });
+
+    await exec.exec('docker-compose', [
+      '--version']).catch(() => { });
+
+    core.startGroup('install docker')
+    await exec.exec('brew', [
+      'cask',
+      'install',
+      DOCKER_CHANNEL !== 'stable' ? 'docker-edge' : 'docker'
+    ]);
+    core.endGroup();
+
+    await exec.exec('mkdir', [
+      '-p',
+      '/Users/runner/.docker'
+    ]);
+
+    await shell(`echo '${DOCKER_DAEMON_JSON}' | sudo tee /Users/runner/.docker/daemon.json`);
+
+    core.startGroup('show daemon json content');
+    await exec.exec('cat', [
+      '/Users/runner/.docker/daemon.json',
+    ]);
+    core.endGroup();
+
+    // allow the app to run without confirmation
+    await exec.exec('xattr', [
+      '-d',
+      '-r',
+      'com.apple.quarantine',
+      '/Applications/Docker.app'
+    ]);
+
+    // preemptively do docker.app's setup to avoid any gui prompts
+    core.startGroup('start docker');
+    await exec.exec('sudo', [
+      'bash',
+      '-c',
+      `
+set -x
+
+sudo /bin/cp /Applications/Docker.app/Contents/Library/LaunchServices/com.docker.vmnetd /Library/PrivilegedHelperTools
+sudo /bin/cp /Applications/Docker.app/Contents/Resources/com.docker.vmnetd.plist /Library/LaunchDaemons/
+sudo /bin/chmod 544 /Library/PrivilegedHelperTools/com.docker.vmnetd
+sudo /bin/chmod 644 /Library/LaunchDaemons/com.docker.vmnetd.plist
+sudo /bin/launchctl load /Library/LaunchDaemons/com.docker.vmnetd.plist
+open -g /Applications/Docker.app || exit
+
+sleep 60
+
+docker info > /dev/null || true
+
+sleep 30
+
+docker info > /dev/null || true
+# Wait for the server to start up, if applicable.
+i=0
+while ! docker system info &>/dev/null; do
+(( i++ == 0 )) && printf %s '-- Waiting for Docker to finish starting up...' || printf '.'
+sleep 1
+done
+(( i )) && printf '\n'
+
+echo "-- Docker is ready."
+`]);
+    core.endGroup();
+
+    core.startGroup('docker version');
+    await exec.exec('docker', ['version']);
+    core.endGroup();
+
+    core.startGroup('docker info');
+    await exec.exec('docker', ['info']);
+    core.endGroup();
+
+    await core.group('set up buildx', buildx);
+
+    return
+  }
+
+  message = 'check docker systemd status';
+  core.startGroup(message)
   await exec.exec('sudo', [
     'systemctl',
     'status',
     'docker',
   ]).then(() => { }).catch(() => { });
+  core.endGroup();
 
-  core.debug('check docker version');
+  message = 'check docker version'
+  core.debug(message);
+  core.startGroup(message);
   await exec.exec('docker', [
     'version',
   ]).catch(() => { });
+  core.endGroup();
 
   if (DOCKER_CHANNEL === 'nightly') {
+    core.startGroup('download deb');
     await exec.exec('curl', [
       '-fsSL',
       '-o',
       '/tmp/moby-snapshot-ubuntu-focal-x86_64-deb.tbz',
       `https://github.com/AkihiroSuda/moby-snapshot/releases/download/${DOCKER_NIGHTLY_VERSION}/moby-snapshot-ubuntu-focal-x86_64-deb.tbz`
     ]);
+    core.endGroup();
 
     await exec.exec('sudo', [
       'rm',
@@ -62,42 +219,54 @@ async function run() {
       '/tmp/*.deb'
     ]);
 
+    core.startGroup('unpack tbz file');
     await exec.exec('tar', [
       'xjvf',
       '/tmp/moby-snapshot-ubuntu-focal-x86_64-deb.tbz',
       '-C',
       '/tmp'
     ]);
+    core.endGroup();
 
+    core.startGroup('update apt cache');
     await exec.exec('sudo', [
       'apt-get',
       'update',
-    ]);
+    ]).catch(() => { });
+    core.endGroup();
 
+    core.startGroup('remove default moby');
     await exec.exec('sudo', [
       'sh',
       '-c',
       "apt remove -y moby-buildx moby-cli moby-containerd moby-engine moby-runc"
     ]).catch(() => { });
+    core.endGroup();
 
+    core.startGroup('install docker');
     await exec.exec('sudo', [
       'sh',
       '-c',
       'apt-get install -y /tmp/*.deb'
     ]).catch(async () => {
+      core.startGroup('download libseccomp2_2.4.3 deb for old os');
       await exec.exec('curl', [
         '-fsSL',
         '-o',
         '/tmp/libseccomp2_2.4.3-1+b1_amd64.deb',
         'http://ftp.us.debian.org/debian/pool/main/libs/libseccomp/libseccomp2_2.4.3-1+b1_amd64.deb'
       ]);
+      core.endGroup();
 
+      core.startGroup('install docker');
       await exec.exec('sudo', [
         'sh',
         '-c',
         'dpkg -i /tmp/*.deb'
       ]);
+      core.endGroup();
     });
+    core.endGroup();
 
   } else {
     core.debug('add apt-key');
@@ -113,20 +282,28 @@ async function run() {
       '/tmp/docker.gpg',
     ]);
 
-    core.debug('add apt source');
+    message = 'add apt source';
+    core.debug(message);
     const UBUNTU_CODENAME = await shell('lsb_release -cs');
+    core.startGroup(message);
     await exec.exec('sudo', [
       'add-apt-repository',
       `deb [arch=amd64] https://download.docker.com/linux/ubuntu ${UBUNTU_CODENAME} ${DOCKER_CHANNEL}`,
     ]);
+    core.endGroup();
 
-    core.debug('update apt cache');
+    message = 'update apt cache'
+    core.debug(message);
+    core.startGroup(message);
     await exec.exec('sudo', [
       'apt-get',
       'update',
-    ]);
+    ]).catch(() => { });
+    core.endGroup();
 
-    core.debug('show available docker version');
+    message = 'show available docker version';
+    core.debug(message);
+    core.startGroup(message);
     await exec.exec('apt-cache', [
       'madison',
       'docker-ce',
@@ -134,6 +311,7 @@ async function run() {
       'grep',
       '19.03'
     ]);
+    core.endGroup()
 
     const DOCKER_VERSION_STRING = await shell(
       `apt-cache madison docker-ce | grep ${DOCKER_VERSION} | head -n 1 | awk '{print $3}' | sed s/[[:space:]]//g`)
@@ -146,7 +324,9 @@ async function run() {
       core.warning(`Docker ${DOCKER_VERSION} not available on ubuntu ${OS}, install latest docker version`);
     }
 
-    core.debug('install docker');
+    message = 'install docker'
+    core.debug(message);
+    core.startGroup(message);
     await exec.exec('sudo', [
       'apt-get',
       '-y',
@@ -154,38 +334,44 @@ async function run() {
       DOCKER_VERSION_STRING ? `docker-ce=${DOCKER_VERSION_STRING}` : 'docker-ce',
       DOCKER_VERSION_STRING ? `docker-ce-cli=${DOCKER_VERSION_STRING}` : 'docker-ce-cli'
     ]);
+    core.endGroup();
   }
 
-  core.debug('check docker version');
+  message = 'check docker version';
+  core.debug(message);
+  core.startGroup(message);
   await exec.exec('docker', [
     'version',
   ]);
+  core.endGroup();
 
-  core.debug('check docker systemd status');
+  message = 'check docker systemd status';
+  core.debug(message);
+  core.startGroup(message);
   await exec.exec('sudo', [
     'systemctl',
     'status',
     'docker',
   ]);
-
-  core.debug('set DOCKER_CLI_EXPERIMENTAL');
-  if (DOCKER_CLI_EXPERIMENTAL === 'enabled') {
-    core.exportVariable('DOCKER_CLI_EXPERIMENTAL', 'enabled');
-  }
+  core.endGroup();
 
   // /etc/docker/daemon.json
   core.debug('set /etc/docker/daemon.json');
+  core.startGroup('show default daemon json content');
   await exec.exec('sudo', [
     'cat',
     '/etc/docker/daemon.json',
   ]);
+  core.endGroup();
 
   await shell(`echo '${DOCKER_DAEMON_JSON}' | sudo tee /etc/docker/daemon.json`);
 
+  core.startGroup('show daemon json content');
   await exec.exec('sudo', [
     'cat',
     '/etc/docker/daemon.json',
   ]);
+  core.endGroup();
 
   await exec.exec('sudo', [
     'systemctl',
@@ -193,50 +379,7 @@ async function run() {
     'docker',
   ]);
 
-  // buildx
-  await exec.exec('docker', [
-    'buildx',
-    'version',
-  ]).then(async () => {
-    if (DOCKER_BUILDX !== 'true') {
-      core.debug('buildx disabled');
-      return;
-    }
-
-    // install buildx
-    await exec.exec('docker', [
-      'run',
-      '--rm',
-      '--privileged',
-      'tonistiigi/binfmt:latest',
-      "--install",
-      "all"
-    ]);
-
-    await exec.exec('ls -la', [
-      '/proc/sys/fs/binfmt_misc',
-    ]);
-
-    await exec.exec('docker', [
-      'buildx',
-      'create',
-      '--use',
-      '--name',
-      'mybuilder',
-      '--driver',
-      'docker-container',
-      '--driver-opt',
-      'image=moby/buildkit:master'
-    ]);
-
-    await exec.exec('docker', [
-      'buildx',
-      'inspect',
-      '--bootstrap'
-    ]);
-  }, () => {
-    core.debug('NOT Support Buildx');
-  });
+  await core.group('set up buildx', buildx);
 }
 
 run().then(() => {
