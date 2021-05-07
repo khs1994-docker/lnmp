008b86a39bb9f90adfe648ca8e76bce92c006f8f
Kang Huaishuai <khs1994@khs1994.com>
2021-05-08 00:59:14 +0800

    [actions] Fixed setup-docker on macOS
    Signed-off-by: Kang Huaishuai <khs1994@khs1994.com>

--- .github/actions/setup-docker/lib/setup-docker.js
+++ .github/actions/setup-docker/lib/setup-docker.js
@@ -161,8 +161,41 @@ async function run() {
       `
 set -x
 
+
+cat <<EOF | tee /tmp/com.docker.vmnetd.plist
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>Label</key>
+	<string>com.docker.vmnetd</string>
+	<key>Program</key>
+	<string>/Library/PrivilegedHelperTools/com.docker.vmnetd</string>
+	<key>ProgramArguments</key>
+	<array>
+		<string>/Library/PrivilegedHelperTools/com.docker.vmnetd</string>
+	</array>
+	<key>RunAtLoad</key>
+	<true/>
+	<key>Sockets</key>
+	<dict>
+		<key>Listener</key>
+		<dict>
+			<key>SockPathMode</key>
+			<integer>438</integer>
+			<key>SockPathName</key>
+			<string>/var/run/com.docker.vmnetd.sock</string>
+		</dict>
+	</dict>
+	<key>Version</key>
+	<string>59</string>
+</dict>
+</plist>
+EOF
+
 sudo /bin/cp /Applications/Docker.app/Contents/Library/LaunchServices/com.docker.vmnetd /Library/PrivilegedHelperTools
-sudo /bin/cp /Applications/Docker.app/Contents/Resources/com.docker.vmnetd.plist /Library/LaunchDaemons/
+# sudo /bin/cp /Applications/Docker.app/Contents/Resources/com.docker.vmnetd.plist /Library/LaunchDaemons/
+sudo /bin/cp /tmp/com.docker.vmnetd.plist /Library/LaunchDaemons/
 sudo /bin/chmod 544 /Library/PrivilegedHelperTools/com.docker.vmnetd
 sudo /bin/chmod 644 /Library/LaunchDaemons/com.docker.vmnetd.plist
 sudo /bin/launchctl load /Library/LaunchDaemons/com.docker.vmnetd.plist
