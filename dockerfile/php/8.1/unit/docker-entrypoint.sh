eacf03985864df772d8943db72014dba9ffcd1b8
Kang Huaishuai <khs1994@khs1994.com>
2021-06-15 15:19:38 +0800

    [PHP] Add 8.1
    Signed-off-by: Kang Huaishuai <khs1994@khs1994.com>

---
+++ dockerfile/php/8.1/unit/docker-entrypoint.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+# copy from https://github.com/nginx/unit/blob/master/pkg/docker/docker-entrypoint.sh
+
+# /var/lib/unit               => /usr/local/nginx-unit/state
+# /var/run/control.unit.sock  => /usr/local/nginx-unit/control.unit.sock
+# /var/run/unit.pid           => /usr/local/nginx-unit/unit.pid
+
+echo "==> Initial Configuration, please see https://unit.nginx.org/installation/#initial-configuration"
+
+set -e
+
+curl_put()
+{
+    RET=`/usr/bin/curl -s -w '%{http_code}' -X PUT --data-binary @$1 --unix-socket /usr/local/nginx-unit/control.unit.sock http://localhost/$2`
+    RET_BODY=${RET::-3}
+    RET_STATUS=$(echo $RET | /usr/bin/tail -c 4)
+    if [ "$RET_STATUS" -ne "200" ]; then
+        echo "$0: Error: HTTP response status code is '$RET_STATUS'"
+        echo "$RET_BODY"
+        return 1
+    else
+        echo "$0: OK: HTTP response status code is '$RET_STATUS'"
+        echo "$RET_BODY"
+    fi
+    return 0
+}
+
+if [ "$1" = "unitd" ]; then
+    if /usr/bin/find "/usr/local/nginx-unit/state/" -mindepth 1 -print -quit 2>/dev/null | /bin/grep -q .; then
+        echo "$0: /usr/local/nginx-unit/state/ is not empty, skipping initial configuration..."
+    else
+        if /usr/bin/find "/docker-entrypoint.d/" -mindepth 1 -print -quit 2>/dev/null | /bin/grep -q .; then
+            echo "$0: /docker-entrypoint.d/ is not empty, launching Unit daemon to perform initial configuration..."
+            /usr/sbin/unitd --user root --group root --log /var/log/nginx-unit/nginx-unit.log --control unix:/usr/local/nginx-unit/control.unit.sock
+
+            while [ ! -S /usr/local/nginx-unit/control.unit.sock ]; do echo "$0: Waiting for control socket to be created..."; /bin/sleep 0.1; done
+            # even when the control socket exists, it does not mean unit has finished initialisation
+            # this curl call will get a reply once unit is fully launched
+            /usr/bin/curl -s -X GET --unix-socket /usr/local/nginx-unit/control.unit.sock http://localhost/
+
+            echo "$0: Looking for certificate bundles in /docker-entrypoint.d/..."
+            for f in $(/usr/bin/find /docker-entrypoint.d/ -type f -name "*.pem"); do
+                echo "$0: Uploading certificates bundle: $f"
+                curl_put $f "certificates/$(basename $f .pem)"
+            done
+
+            echo "$0: Looking for configuration snippets in /docker-entrypoint.d/..."
+            for f in $(/usr/bin/find /docker-entrypoint.d/ -type f -name "*.json"); do
+                echo "$0: Applying configuration $f";
+                curl_put $f "config"
+            done
+
+            echo "$0: Looking for shell scripts in /docker-entrypoint.d/..."
+            for f in $(/usr/bin/find /docker-entrypoint.d/ -type f -name "*.sh"); do
+                echo "$0: Launching $f";
+                "$f"
+            done
+
+            # warn on filetypes we don't know what to do with
+            for f in $(/usr/bin/find /docker-entrypoint.d/ -type f -not -name "*.sh" -not -name "*.json" -not -name "*.pem"); do
+                echo "$0: Ignoring $f";
+            done
+
+            echo "$0: Stopping Unit daemon after initial configuration..."
+            kill -TERM `/bin/cat /usr/local/nginx-unit/unit.pid`
+
+            while [ -S /usr/local/nginx-unit/control.unit.sock ]; do echo "$0: Waiting for control socket to be removed..."; /bin/sleep 0.1; done
+
+            echo
+            echo "$0: Unit initial configuration complete; ready for start up..."
+            echo
+        else
+            echo "$0: /docker-entrypoint.d/ is empty, skipping initial configuration..."
+        fi
+    fi
+fi
+
+exec "$@"
