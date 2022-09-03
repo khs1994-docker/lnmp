ae9f6bf0ba6f829017abccab9836d26c5e5aa7ce
Kang Huaishuai <khs1994@khs1994.com>
2022-09-03 22:53:45 +0800

    [MySQL] Update default config
    Signed-off-by: Kang Huaishuai <khs1994@khs1994.com>

--- config/default.sh
+++ config/default.sh
@@ -26,7 +26,6 @@ docker cp config_php7_1_666:/usr/local/etc php/default/usr/local/
 docker cp config_php8_1_666:/usr/local/etc php8/default/usr/local/
 
 docker cp config_nginx_1_666:/etc/nginx/nginx.conf etc/default/nginx/
-
 docker cp config_nginx_1_666:/etc/nginx/conf.d etc/default/nginx/
 
 docker cp config_httpd_1_666:/usr/local/apache2/conf/httpd.conf etc/default/apache2/httpd.conf
@@ -34,6 +33,7 @@ docker cp config_httpd_1_666:/usr/local/apache2/conf/httpd.conf etc/default/apac
 docker cp config_mariadb_1_666:/etc/mysql mariadb/default/etc/
 
 docker cp config_mysql_1_666:/etc/mysql mysql/default/etc/
+docker cp config_mysql_1_666:/etc/my.cnf mysql/default/etc/
 
 docker compose down
 
