298c5127af56a4d6637ceeb5b17bb845fab5e872
Kang Huaishuai <khs1994@khs1994.com>
2024-02-10 10:20:02 +0800

    [lwpm] Update soft version
    Signed-off-by: Kang Huaishuai <khs1994@khs1994.com>

--- scripts/issue.tls.sh
+++ scripts/issue.tls.sh
@@ -4,25 +4,21 @@ if [ $1 = 'rsa' ];then
   lnmp-docker ssl khs1994.com -d *.khs1994.com \
                      -d *.developer.khs1994.com \
                      -d *.home.khs1994.com \
-                     -d *.baidu.khs1994.com \
-                     -d *.alibaba.khs1994.com \
-                     -d *.tencent.khs1994.com \
                      -d *.cxyl.khs1994.com \
                      -d *.ci.khs1994.com \
                      -d *.ci2.khs1994.com \
                      -d *.mirrors.khs1994.com -d *.mirror.khs1994.com \
                      -d docker-practice.com -d *.docker-practice.com \
+                     -d local.khs1994.com -d *.local.khs1994.com \
                      --rsa
 fi
 
 lnmp-docker ssl khs1994.com -d *.khs1994.com \
                    -d *.developer.khs1994.com \
                    -d *.home.khs1994.com \
-                   -d *.baidu.khs1994.com \
-                   -d *.alibaba.khs1994.com \
-                   -d *.tencent.khs1994.com \
+                   -d *.cxyl.khs1994.com \
                    -d *.ci.khs1994.com \
                    -d *.ci2.khs1994.com \
                    -d *.mirrors.khs1994.com -d *.mirror.khs1994.com \
                    -d docker-practice.com -d *.docker-practice.com \
-                   -d *.cxyl.khs1994.com
+                   -d local.khs1994.com -d *.local.khs1994.com
