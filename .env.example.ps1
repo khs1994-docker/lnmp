bf6a57f3cdff5e57f6ad3aa594a6efc0dc097714
Kang Huaishuai <khs1994@khs1994.com>
2025-10-03 09:32:37 +0800

    [skip ci] update .env.example.ps1
    Signed-off-by: Kang Huaishuai <khs1994@khs1994.com>

--- .env.example.ps1
+++ .env.example.ps1
@@ -1,24 +1,27 @@
 # 本项目全局变量
 
 # 是否为国内环境，默认为 true，当处于非国内环境(例如：GitHub Actions)时请改为 false
-${env:LNMP_CN_ENV}=$true # $true or $false
+if (!(Test-Path env:LNMP_CN_ENV)) {
+  ${env:LNMP_CN_ENV} = $true
+  # ${env:LNMP_CN_ENV}=$false
+}
 
 # end
 
 # You can overwrite this file in .env.ps1
 
-$CI_HOST="ci.khs1994.com:1218"
+$CI_HOST = "ci.khs1994.com:1218"
 
 # wsl name
 # $ wslconfig /l
 # $ wsl -l
-$DistributionName="Ubuntu-22.04"
+$DistributionName = "Ubuntu-22.04"
 
-$NGINX_PATH="C:/nginx"
-$PHP_PATH="C:/php"
+$NGINX_PATH = "C:/nginx"
+$PHP_PATH = "C:/php"
 
 # used by $ lnmp-wnamp start common
-$COMMON_SOFT="nginx","php","mysql","wsl-redis"
+$COMMON_SOFT = "nginx", "php", "mysql", "wsl-redis"
 
 # $LNMP_CACHE=
 
